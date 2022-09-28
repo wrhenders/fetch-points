8865b8e1060790e6362bd5d6be7b2eb71c4cb0a4
Ryan Henderson <wrhenders@gmail.com>
2022-09-28 15:20:10 -0500

    business logic for repo

--- components/repo.ts
+++ components/repo.ts
@@ -1,17 +1,15 @@
 import fs from "fs";
-import { Transaction, Payer } from "../interfaces";
 
 // get data from JSON db
 const db = require("../db.json");
-
-let log: Transaction[] = db.log;
-let currentBalance: number = db.currentBalance;
+let log: [{ payer: string; points: number; date: string; seen: boolean }] =
+  db.log;
 let pointsMap = db.pointsMap;
 
 export const repo = {
   getBalance,
   newTransaction,
-  //newSpend,
+  newSpend,
 };
 
 // get balance: returns current pointsMap
@@ -20,43 +18,88 @@ function getBalance() {
 }
 
 // newTransaction: takes payer, points and date, validates,
-// appends to log, sorts based on timestamp, updates pointsMap & currentBalance
+// appends to log, sorts based on timestamp, updates pointsMap
 // writes to db
 function newTransaction(payer: string, points: number, date: string) {
-  const data: Transaction = { payer, points, date };
-  log.push(data);
+  log.push({ payer, points, date, seen: false });
+
   log.sort((a, b) => {
     return new Date(a.date).getTime() - new Date(b.date).getTime();
   });
+
   pointsMap[payer] ? (pointsMap[payer] += points) : (pointsMap[payer] = points);
-  currentBalance += points;
+  if (pointsMap[payer] < 0) pointsMap[payer] = 0;
+
   saveData();
 }
 
 // spend: takes point to spend, validates enough points
-// create spentPoints[]
-// for (let payer of log)
-//  pointsToSpend > 0
-// if pointsToSpend >= payer.points
-// add payer to spentPoints
-// pointsToSpend -= payer.points
-// payer.points = 0
-// payer has more points than pointsToSpend
-// add partial payer to spentPoints
-// payer.points -= pointsToSpend
-// pointsToSpend = 0
-// create ret{}
-// for (let transaction of spentPoints)
-// if(ret[!transaction.payer]) ret[transaction.payer]=0
-// ret[transaction.payer] -= trasaction.points
-// currentBalance -= transaction.points
-// pointsMap[transaction.payer] -= transaction.points
-// return ret{}
-// writes to db
+function newSpend(pointsToSpend: number) {
+  let balance = currentBalance();
+  if (pointsToSpend > balance) {
+    throw new Error("Not enough points");
+  }
 
+  const spentPoints: { [payer: string]: number } = {};
+  for (let tx of log) {
+    // if tx seen and used, continue
+    console.log(tx, pointsToSpend, spentPoints);
+    if (tx.seen) continue;
+
+    if (pointsToSpend > 0) {
+      // if spending points is more than payer points and the tx has positive points
+      // add tx and reduce spending points by tx amout and mark seen
+      if (pointsToSpend >= tx.points && tx.points > 0) {
+        spentPoints[tx.payer]
+          ? (spentPoints[tx.payer] -= tx.points)
+          : (spentPoints[tx.payer] = -tx.points);
+        pointsMap[tx.payer] -= tx.points;
+        pointsToSpend -= tx.points;
+        tx.seen = true;
+        // else if spending points is more than payer points and the tx has negative points
+        // check if payer has points
+      } else if (pointsToSpend >= tx.points && tx.points < 0) {
+        if (spentPoints[tx.payer]) {
+          if (spentPoints[tx.payer] - tx.points < 0) {
+            spentPoints[tx.payer] -= tx.points;
+            pointsMap[tx.payer] -= tx.points;
+            pointsToSpend -= tx.points;
+            tx.seen = true;
+          } else {
+            continue;
+          }
+        }
+        // else if spending points is less than payer points on tx
+        // add payer with remaining pointsToSpend, reduce points left in tx
+      } else if (pointsToSpend < tx.points) {
+        spentPoints[tx.payer]
+          ? (spentPoints[tx.payer] -= pointsToSpend)
+          : (spentPoints[tx.payer] = -pointsToSpend);
+        pointsMap[tx.payer] -= pointsToSpend;
+        tx.points -= pointsToSpend;
+        pointsToSpend = 0;
+      }
+    }
+  }
+
+  // save data
+  saveData();
+
+  // convert hashmap to array format and return
+  const returnArr = [];
+  for (let payer in spentPoints) {
+    returnArr.push({ payer, points: spentPoints[payer] });
+  }
+  return returnArr;
+}
+
+function currentBalance() {
+  let balance = 0;
+  for (let payer in pointsMap) {
+    balance += pointsMap[payer];
+  }
+  return balance;
+}
 function saveData() {
-  fs.writeFileSync(
-    "db.json",
-    JSON.stringify({ log, currentBalance, pointsMap }, null, 2)
-  );
+  fs.writeFileSync("db.json", JSON.stringify({ log, pointsMap }, null, 2));
 }
